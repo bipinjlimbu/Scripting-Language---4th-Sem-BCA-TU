51c01c8c0251eec573e8dc1596564c94d5902521
bipinjlimbu <bipinlimbu712@gmail.com>
2025-08-22 20:06:19 +0545

    Data Types in JS

---
+++ JavaScript/02_DataTypes.js
@@ -0,0 +1,20 @@
+//String
+let userName = "Bipin Limbu"
+
+//Nunber
+let number = 7;
+
+//Boolean
+let isPass = true;
+
+//Undefiend
+let Hahaha;
+
+//Object
+const std = {"Bipin":19,"Ishan":"Geh"}
+
+console.log(userName+" is "+typeof(userName));
+console.log(number+" is "+typeof(number));
+console.log(isPass+" is "+typeof(isPass));
+console.log(Hahaha+" is "+typeof(Hahaha));
+console.log(std+" is "+typeof(std));
