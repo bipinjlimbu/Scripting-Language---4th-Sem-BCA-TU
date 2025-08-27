2e06e144f03406cf6a2f0440a1ede359355a07f6
bipinjlimbu <bipinlimbu712@gmail.com>
2025-08-27 07:54:40 +0545

    Problem 2 of JavaScript Completed

---
+++ Classwork/Problem2.js
@@ -0,0 +1,10 @@
+//WAP to check triangle validity using sides
+let a = 7;
+let b = 8;
+let c = 9;
+if(a+b>c && b+c>a && c+a>b){
+    console.log("Triangle is Valid")
+}
+else{
+    console.log("Triangle is not Valid")
+}
