920c6427f097fe3072ba4a3dfd139004fde0c596
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-03 07:09:29 +0545

    For Loop in JS

--- JavaScript/05_Loops.js
+++ JavaScript/05_Loops.js
@@ -4,4 +4,15 @@ let n = 7;
 for(let i = 1;i<=n;i++){
     sum += i;
 }
+console.log("For Loop");
 console.log(`Sum:${sum}`);
+
+//For-in Loop
+const obj = {
+    name:"Bipin",
+    age:19
+};
+console.log("\nFor-in Loop");
+for (let i in obj){
+    console.log(`${i} : ${obj[i]}`)
+}
