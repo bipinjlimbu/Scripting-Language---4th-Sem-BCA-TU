764a0390dd6a43ad381c49622c18c81e9ff95f76
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-04 07:57:09 +0545

    Labwork 5 Completed in JS

---
+++ Labwork/Labwork5.js
@@ -0,0 +1,7 @@
+//WAP to find BMI using function
+function calculateBMI(weight,height){
+    let BMI = weight/height;
+    return BMI;
+}
+console.log(`Body Mass Index of Bipin:${calculateBMI(69,1.82)}`)
+console.log(`Body Mass Index of Ruksan:${calculateBMI(59,1.82)}`)
