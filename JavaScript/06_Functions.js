4a4bbba063edb799f71d35c2c2fe8a56a8caf13a
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-04 07:00:34 +0545

    Arrow Functions Completed in JS

--- JavaScript/06_Functions.js
+++ JavaScript/06_Functions.js
@@ -1,9 +1,14 @@
+//Normal Function
 function show(n){
     let sum = 0;
     for(let i = 1;i<=n;i++){
         sum += i;
     }
-    return sum
+    return `${sum}\n`
 }
 console.log(show(7));
 console.log(show(12));
+
+//Arrow Function
+const sum=(x,y)=>x+y;
+console.log(sum(4,3));
