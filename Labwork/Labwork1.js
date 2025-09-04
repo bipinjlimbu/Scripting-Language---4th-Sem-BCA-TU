b60889f1ad47a2c8dbf653bf77c1812ad4eda1c5
bipinjlimbu <bipinlimbu712@gmail.com>
2025-09-04 07:47:54 +0545

    Labwork 1 Completed in JS

---
+++ Labwork/Labwork1.js
@@ -0,0 +1,6 @@
+//WAP to print "Hello World" with brief introduction of JS and JS coding Conventions
+console.log("Hello World");
+console.log("JavaScript");
+console.log("JS is a lightweight, dynamic programming language primarily used to add interactivity and dynamic behavior to web pages. It works alongside HTML for structuring content and CSS for styling. Beyond web browsers, JavaScript is also used in server-side development (Node.js), mobile app development, and more.")
+console.log("JavaScript Coding Conventions:");
+console.log("Coding conventions are a set of guidelines for writing clean, readable, and maintainable code. Adhering to conventions promotes consistency and collaboration within development teams. Common JavaScript coding conventions include:")
