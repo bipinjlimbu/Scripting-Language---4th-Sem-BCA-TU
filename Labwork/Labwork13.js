0dd8248fcd5244970d984db69a8922aee4d3fd2a
bipinjlimbu <bipinlimbu712@gmail.com>
2025-11-05 19:15:09 +0545

    Scripting Language Labwork 13 Completed

---
+++ Labwork/Labwork13.js
@@ -0,0 +1,11 @@
+let students =[{Name:"Bipin",Age:"19"},
+    {Name:"Anush",Age:"20"},
+    {Name:"Ishan",Age:"19"},
+    {Name:"Ruksan",Age:"21"}
+]
+
+let age = students.map((v)=>{
+    return v.Age;
+})
+
+console.log(age);
