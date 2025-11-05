7b19dda1830053763aa6e49a2577e4cd7047fb1f
bipinjlimbu <bipinlimbu712@gmail.com>
2025-11-05 19:14:16 +0545

    Scripting Language Labwork 10 Completed

---
+++ Labwork/Labwork10.js
@@ -0,0 +1,7 @@
+let books =[{Title:"Book1",Author:"Author1"},
+    {Title:"Book2",Author:"Author2"},
+    {Title:"Book3",Author:"Author3"},
+    {Title:"Book4",Author:"Author4"}
+]
+books.splice(1,1);
+console.log(books);
