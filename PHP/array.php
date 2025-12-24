1f2273dfbab45725b144870204ec229ae544380d
bipinjlimbu <bipinlimbu712@gmail.com>
2025-12-24 07:08:13 +0545

    Associative Array in PHP

--- PHP/array.php
+++ PHP/array.php
@@ -7,4 +7,16 @@ foreach ($data1 as $value) {
     echo "$value <br> ";
 }
 
+// Associative Array
+echo "<h3>Associative Array</h3>";
+$data2 = [
+    "name" => "Bipin",
+    "age" => 19,
+    "salary" => 69000.50,
+    "is_active" => true
+];
+
+foreach ($data2 as $key => $value) {
+    echo "$key : $value <br> ";
+}
 ?>
