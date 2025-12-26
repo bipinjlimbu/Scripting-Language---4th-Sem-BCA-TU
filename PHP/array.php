442c259e57e7615e74e74826a914298e070d0237
bipinjlimbu <bipinlimbu712@gmail.com>
2025-12-26 07:07:15 +0545

    Multidimensional Array in PHP

--- PHP/array.php
+++ PHP/array.php
@@ -19,4 +19,18 @@ $data2 = [
 foreach ($data2 as $key => $value) {
     echo "$key : $value <br> ";
 }
+
+//Multidimensional Array
+echo "<h3>Multidimensional Array</h3>";
+$data3 = [
+    ["Bipin", 19, 69000.50, true],
+    ["Anush", 22, 75000.00, false],
+    ["Ishan", 19, 80000.75, true]
+];
+foreach ($data3 as $mdArray) {
+    foreach ($mdArray as $value) {
+        echo "$value ";
+    }
+    echo "<br>";
+}
 ?>
