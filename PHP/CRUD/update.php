0cf273c07d238db75196e9d7389b5721a37440c8
bipinjlimbu <bipinlimbu712@gmail.com>
2026-01-19 19:21:42 +0545

    Update Operation in PHP CRUD

---
+++ PHP/CRUD/update.php
@@ -0,0 +1,70 @@
+<?php
+include 'connectdb.php';
+$id = $_GET['updateid'];
+$sql = "SELECT * from formvalidation where id=$id";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+$name = $row['name'];
+$email = $row['email'];
+$mobile = $row['mobile'];
+$password = $row['password'];
+
+if (isset($_POST['update'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $password = $_POST['password'];
+
+    $sql = "UPDATE formvalidation set id=$id, name='$name', email='$email', 
+    mobile='$mobile', password='$password' where id=$id";
+
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        header('location:display.php');
+    } else {
+        die(mysqli_error($conn));
+    }
+}
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+
+    <div class="container my-5">
+        <form method="post">
+            <div class="mb-3">
+                <label>Name</label>
+                <input type="text" class="form-control" placeholder="Enter your name" name="name"
+                    value=<?php echo $name; ?>>
+            </div>
+            <div class=" mb-3">
+                <label>Email</label>
+                <input type="email" class="form-control" placeholder="Enter your email" name="email"
+                    value=<?php echo $email; ?>>
+            </div>
+            <div class="mb-3">
+                <label>Mobile</label>
+                <input type="text" class="form-control" placeholder="Enter your mobile number" name="mobile"
+                    value=<?php echo $mobile; ?>>
+            </div>
+            <div class=" mb-3">
+                <label>Password</label>
+                <input type="password" class="form-control" placeholder="Enter your password" name="password"
+                    value=<?php echo $password; ?>>
+            </div>
+            <button type=" submit" class="btn btn-primary" name="update">Update</button>
+        </form>
+    </div>
+
+</html>
