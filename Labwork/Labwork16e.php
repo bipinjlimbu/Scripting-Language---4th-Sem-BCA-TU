f223f0efe7fe21e583df44f219210332adcfece7
bipinjlimbu <bipinlimbu712@gmail.com>
2026-01-21 10:06:32 +0545

    Labwork 16 e Completed

---
+++ Labwork/Labwork16e.php
@@ -0,0 +1,29 @@
+<?php
+$choice = 2;
+
+switch ($choice) {
+    case 1:
+        echo "Today is Sunday.";
+        break;
+    case 2:
+        echo "Today is Monday.";
+        break;
+    case 3:
+        echo "Today is Tuesday.";
+        break;
+    case 4:
+        echo "Today is Wednesday.";
+        break;
+    case 5:
+        echo "Today is Thursday.";
+        break;
+    case 6:
+        echo "Today is Friday.";
+        break;
+    case 7:
+        echo "Today is Saturday.";
+        break;
+    default:
+        echo "Invalid choice.";
+}
+?>
