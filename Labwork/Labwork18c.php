a1768e87dc4ea5ab2cf7064aee3c9e903ffca1c2
bipinjlimbu <bipinlimbu712@gmail.com>
2026-01-21 10:10:37 +0545

    Labwork 18 c Completed

---
+++ Labwork/Labwork18c.php
@@ -0,0 +1,5 @@
+<?php
+date_default_timezone_set('UTC');
+echo "Current date and time (YYYY-MM-DD HH:MM:SS): " . date('Y-m-d H:i:s') . "<br>";
+echo "Current date and time (Day of the week, Month DD, YYYY): " . date('l, F d, Y');
+?>
